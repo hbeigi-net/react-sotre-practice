f25eb750d32e4e1ad0424b7bf6271336843da3fb
hamid <hbeigi57@gmail.com>
2022-03-01 15:35:37 +0330

    product carOne ui

---
+++ src/components/productCard-1.jsx
@@ -0,0 +1,96 @@
+import React from 'react'
+import styled , {createGlobalStyle} from "styled-components"
+import Rating from '@mui/material/Rating';
+import {BtnOne} from "../components/subComponents/btns"
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+const PcOneStyles = createGlobalStyle`
+    /* pc stands for Product Card  */
+    #pcOne 
+    {
+        margin : 100px ; 
+        max-width : 234px ; 
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 10px 25px;
+        position: relative;
+        /* border: 1px solid #CACDD8; */
+        box-shadow: rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;
+         overflow :hidden ; 
+    }
+    #pcOneContent
+    {
+        width :100% ; 
+    }
+    #pcOne #pcOneContent img 
+    {
+        width : 100% ;
+        display : block ; 
+        margin-left : auto ; 
+        margin-right : auto ; 
+    }
+    #pcOne #pcOneContent #pcOneReviews
+    {
+        margin : 10px 0px ; 
+        display : flex ; 
+        align-items :center ; 
+        justify-content : space-between ;
+        font-size:12px ; 
+        color : #0e0d0da0;
+    }
+    #pcOne #pcOneContent #pcOneTitle
+    {
+        font-size : 13px ;
+        line-height : 13px ; 
+        font-weight :400 ;  
+    }
+    #pcOne #pcOneContent #noDiscountPrice
+    {
+        text-decoration : line-through;
+        color : grey ; 
+        font-size : 12px ;
+        font-weight :400 ;
+        line-height : 12px ; 
+        margin-top :5px ;   
+        
+    }
+    #pcOne #pcOneContent #pcOnePride
+    {
+        font-size : 14px ;
+        font-weight :400 ; 
+    }
+`
+export default function PcOne() {
+  return (
+    <>
+    <PcOneStyles/>
+    <div id="pcOne">
+        <div id="pcOneContent">
+
+                
+                <img src='./—Pngtree—wireless headphones blue_4347448.png'/>
+                
+                <div id="pcOneReviews">
+                      <Rating name="read-only" value={4.4}  readOnly  size="small" precision={0.1} />
+                      Reviews (<span>34</span>)
+                </div>
+
+                <h3 id="pcOneTitle">
+                    EX DISPLAY : MSI Pro 16 Flex-036AU 15.6 MULTITOUCH All-In-On...
+                </h3>
+
+                <div id="pcOnePriceArea">
+                    <p id = "noDiscountPrice">$499.00</p>
+                    <p className='pcOnePride'>$480.00</p>
+                </div>
+                <BtnOne style={{ display : "flex" , alignItems : "center" , justifyContent : 'center' , padding :'3px 10px ' , margin : "4px"}}>
+                    <AddShoppingCartIcon sx={{fontSize : "16px" , mr : "10px"}}/>
+                     Add to Cart  
+                </BtnOne>
+
+
+        </div>
+    </div>
+    </>
+  )
+}
--- src/pages/store.jsx
+++ src/pages/store.jsx
@@ -4,13 +4,14 @@ import React,{useEffect} from 'react'
 // import Nav from "../components/nav"
 // import TopBar from "../components/topBar"
 // import ADDrop from "../components/account-1"
-import PlaceholderOne from "../components/placeholder-1"
-import PlaceholderTwo from "../components/placeholder-2"
+// import PlaceholderOne from "../components/placeholder-1"
+// import PlaceholderTwo from "../components/placeholder-2"
+import PcOne from '../components/productCard-1'
 export default function Store() {
 
   return (
     <>
-
+      <PcOne/>
     </>
   )
 }
