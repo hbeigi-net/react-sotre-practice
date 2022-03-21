ecc3a4122666bb4626093dc0801185bbce54f851
hamid <hbeigi57@gmail.com>
2022-03-21 17:12:33 +0330

    compeleter rest in  product info  containing  main footer

--- src/components/productInfoComponents/main.jsx
+++ src/components/productInfoComponents/main.jsx
@@ -1,10 +1,12 @@
 
 import React from 'react'
 import Rest from "./rest"
+import MainFooter from "../mainFooter"
 export default function ProductInfo() {
   return (  
       <>
         <Rest/>
+        <MainFooter />
       </>
   )
 }
--- src/components/productInfoComponents/rest.jsx
+++ src/components/productInfoComponents/rest.jsx
@@ -3,14 +3,17 @@ import React from 'react'
 import {createGlobalStyle} from "styled-components"
 import Grid from '@mui/material/Grid'
 
+
 const RestStyles = createGlobalStyle`
     .restCotainer
     {
         width : 95vw; 
         max-width : 1250px ; 
+        margin : 0 auto ; 
+        font-size : 13px ; 
     }
     .bgDark{
-        background-color : #000000;
+        background-color :#111;
         color : white ; 
     }
     .restHead
@@ -37,14 +40,67 @@ const RestStyles = createGlobalStyle`
         justify-content : center ; 
         flex-direction : column ; 
     }
+    .restFlexAround
+    {
+        display : flex ; 
+        align-items : center ;
+        justify-content : space-around
+    }
     .restSection
     {
-        margin : 30px 0 ; 
+        margin : 30px auto ; 
         padding : 50px ; 
+
     }
     .restImgFluid
     {
         max-width : 100% ; 
+        height : 100% ; 
+    }
+    .restSupportActions ul li
+    {
+        border-radius : 2px ; 
+        box-shadow: rgba(0, 0, 0, 0.05) 0px 6px 24px 0px, rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;       
+         padding : 3px 100px 3px 10px ;
+        margin : 15px ; 
+        font-size : 13px ; 
+        font-weight : 400 ; 
+        cursor: pointer; 
+           }
+
+    .restSupportImage
+    {
+        margin : 0 ; 
+        padding : 0 ; 
+        height : 400px ; 
+        position : relative ; 
+        overflow : hidden ; 
+  
+    }
+    .restPad-1
+    {
+        padding : 2rem 1rem ; 
+    }
+    .restTextContent h2{
+        margin-bottom : 10px ; 
+    }
+    .restTextContent
+    {
+        max-width : 500px ; 
+        margin : 0 auto ; 
+        text-align : center ; 
+    }
+    .restCenter
+    {
+        margin : 0 auto ; 
+        text-align : center ; 
+    }
+    .restOutStand
+    {
+        font-weight :600 ;
+        letter-spacing : 1px ; 
+        font-size : 15px ;  
+        margin : 0 5px ; 
     }
 `
 export default function Rest() {
@@ -54,7 +110,7 @@ export default function Rest() {
             <RestStyles/>
 
             <div className="restContainerFluid bgDark">
-                <div className="restContainer restSection ">
+                <div className="restCotainer restSection ">
                     <Grid container spacing={2} justifyContent ="center" alignItems={"center"}>
                         <Grid item md={5}>
                             <div className=" ">
@@ -74,6 +130,99 @@ export default function Rest() {
                     </Grid> 
                 </div>
             </div>
+
+            <div className="restCotainer">
+                <div className="restFlexAround">
+                    <div className="restSupportActions">
+                            <ul>
+                                <li>
+                                    Product Support 
+                                </li>
+                                <li>
+                                    FAQ
+                                </li>
+                                <li>
+                                    Our Buyer Guide  
+                                </li>
+                            </ul>
+                    </div>
+                    <div className="restSupportImage">
+                        <img src="./img/rest/restSupport.png"  alt="support information" className='restImgFluid' />
+                    </div>
+                </div>
+            </div>
+            <div className="restContainerFluid bgDark">
+                <div className="restCotainer">
+                    <div className="restFlexColumn restSection restTextContent ">
+                        <h2>
+                            Features
+                        </h2>
+                        <p>
+                        The MPG series brings out the best in gamers by allowing full expression in color with advanced RGB lighting control and synchronization.
+                        </p>
+                    </div>
+                    <div className="restInfoArea restPad-1">
+                            <Grid container spacing={2}>
+
+
+                              <Grid item xs = {6}  sm ={3}>
+                                  <div className="restFlexColumn restCenter">
+                                      <img src="./img/rest/Group 14.png" alt="asset" className="restImgFluid" />
+                                      <div className="">
+                                          <span className="restOutStand">
+                                              Intel core i7 
+                                          </span>
+                                     processor with the upmost computing power to bring you an unparalleled gaming experience.
+                                      </div>
+                                  </div>
+                              </Grid>
+
+
+                              <Grid item xs = {6}  sm ={3}>
+                                  <div className="restFlexColumn restCenter">
+                                      <img src="./img/rest/Group 17.png" alt="asset" className="restImgFluid" />
+                                      <div className="">
+                                          <span className="restOutStand">
+                                              Intel core i7 
+                                          </span>
+                                     processor with the upmost computing power to bring you an unparalleled gaming experience.
+                                      </div>
+                                  </div>
+                              </Grid>
+
+
+                              <Grid item xs = {6}  sm ={3}>
+                                  <div className="restFlexColumn restCenter">
+                                      <img src="./img/rest/Group 16.png" alt="asset" className="restImgFluid" />
+                                      <div className="">
+                                          <span className="restOutStand">
+                                              Intel core i7 
+                                          </span>
+                                     processor with the upmost computing power to bring you an unparalleled gaming experience.
+                                      </div>
+                                  </div>
+                              </Grid>
+
+
+                              <Grid item xs = {6}  sm ={3}>
+                                  <div className="restFlexColumn restCenter">
+                                      <img src="./img/rest/Group 15.png" alt="asset" className="restImgFluid" />
+                                      <div className="">
+                                          <span className="restOutStand">
+                                              Intel core i7 
+                                          </span>
+                                     processor with the upmost computing power to bring you an unparalleled gaming experience.
+                                      </div>
+                                  </div>
+                              </Grid>
+
+
+                            </Grid>
+
+                    </div>
+                </div>
+            </div>
+
         </>
   )
 }
