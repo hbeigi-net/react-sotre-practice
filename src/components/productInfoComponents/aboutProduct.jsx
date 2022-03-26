c10e9d910871a502652b76970967c950dad75dd1
hamid <hbeigi57@gmail.com>
2022-03-26 16:48:10 +0330

    product info component end

--- src/components/productInfoComponents/aboutProduct.jsx
+++ src/components/productInfoComponents/aboutProduct.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function AboutProduct() {
   return (
-    <div>AboutProduct</div>
+    <div>
+        <h2>
+        MSI MPG Trident 3
+        </h2>
+        <p>
+        MSI MPG Trident 3 10SC-005AU Intel i7 10700F, 2060 SUPER, 16GB RAM, 512GB SSD, 2TB HDD, Windows 10 Home, Gaming Keyboard and Mouse 3 Years Warranty Gaming Desktop
+        </p>  
+    </div>
   )
 }
--- src/components/productInfoComponents/productDetail.jsx
+++ src/components/productInfoComponents/productDetail.jsx
@@ -2,6 +2,25 @@ import React from 'react'
 
 export default function ProductDetail() {
   return (
-    <div>ProductDetail</div>
+    <div>
+      <h2>
+      MSI MPG Trident 3
+      </h2>
+      <ul>
+      <li>•  Intel Core i7-10700F</li>
+      <li>•  Intel H410</li>
+      <li>•  WHITE</li>
+      <li>•  NVIDIA MSI GeForce RTX 2060 SUPER 8GB AERO ITX GDDR6</li>
+      <li>•  SO-DIMM 16GB (16GB x 1) DDR4 2666MHz</li>
+      <li>•  2 total slots (64GB Max)</li>
+      <li>•  512GB (1 x 512GB) M.2 NVMe PCIe GEN3x4 SSD 2TB (2.5) 5400RPM</li>
+      <li>•  Gaming Keyboard GK30 + Gaming Mouse GM11</li>
+      <li>•  3.5 HDD (0/0), 2.5 HDD/SSD(1/0), M.2 (1/0)</li>
+      <li>•  Intel WGI219Vethernet (10/100/1000M)</li>
+      <li>•  AX200 (WIFI 6)+BT5.1</li>
+      <li>•  PSU 330W</li>
+      <li>•  Fan Cooler</li>
+            </ul>
+    </div>
   )
 }
